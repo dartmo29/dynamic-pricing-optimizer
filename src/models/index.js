9c8bad6111a4d2a96215c3330be8e86a0cdb534b
dartmo29 <ecnicholas7@gmail.com>
2025-03-23 21:25:05 -0500

    Add models index file for easier imports

---
+++ src/models/index.js
@@ -0,0 +1,12 @@
+/**
+ * Models index file
+ * Export all models for easy importing
+ */
+
+import CostModel from './CostModel';
+import PricingModel from './PricingModel';
+
+export {
+  CostModel,
+  PricingModel
+};
