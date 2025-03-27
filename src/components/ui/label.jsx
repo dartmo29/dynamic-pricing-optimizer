f0af6e95ab55d9e83d2cf8ca2cc2345410ee961c
dartmo29 <ecnicholas7@gmail.com>
2025-03-26 19:59:45 -0500

    Fix Label component import path

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,20 +1,33 @@
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva } from "class-variance-authority"
+/**
+ * label.jsx
+ * Label component based on Radix UI Primitives
+ */
 
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva } from "class-variance-authority";
+
+import { cn } from "../../utils/cn";
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+);
 
+/**
+ * Label component
+ * 
+ * @param {Object} props Component props
+ * @param {string} props.className Custom CSS classes
+ * @param {...any} props.props Other props
+ * @returns {JSX.Element} Styled label component
+ */
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
     className={cn(labelVariants(), className)}
     {...props}
   />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+));
+Label.displayName = LabelPrimitive.Root.displayName;
 
-export { Label }
+export { Label };
